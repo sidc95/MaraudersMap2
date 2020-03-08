eceef5098d8950c028b455df52e50296361179b0
Sid <sid@Sids-MBP.fios-router.home>
2020-03-08 18:40:26 -0400

    New Stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Button, FormControl, Nav, Form } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-        <p>
-          Hello World!
-        </p>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Mauraders Map</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Restaurants</Nav.Link>
+          <Nav.Link href="#features">Reviews</Nav.Link>
+          <Nav.Link href="#pricing">About</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+      <div className="Message">
+        This is a simple restaurant app!
+      </div>
     </div>
   );
 }
